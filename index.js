49e06e19991fc9384532a9274c2dae160d8f4242
Kaizurc <118689910+Shempong@users.noreply.github.com>
2023-10-27 12:38:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById("nav-btn");
+    const sidenav = document.querySelector(".sidenav");
+    const cancelBtn = document.querySelector(".cancel-btn");
 
+    menuButton.addEventListener("click", function () {
+        sidenav.classList.toggle("show");
+    });
+
+    cancelBtn.addEventListener("click", function () {
+        sidenav.classList.remove("show");
+    });
+});
